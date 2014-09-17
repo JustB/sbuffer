db35daaaf5a5fa2ba2fec5256af1aa9ae41f8654
Giustino Borzacchiello <giustinob@gmail.com>
2014-09-18 00:20:05 +0200

    Added create method

--- src/Justb/LinksBundle/Controller/DefaultController.php
+++ src/Justb/LinksBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace Justb\LinksBundle\Controller;
 
+use Justb\LinksBundle\Entity\Link;
+use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -9,5 +12,19 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return $this->render('JustbLinksBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function createAction()
+    {
+        $link = new Link();
+        $link->setTitle('How to Give a Talk');
+        $link->setUrl('https://zapier.com/blog/how-give-talk/');
+        $link->setDescription('Learn how to give a talk and what errors you should avoid');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($link);
+        $em->flush();
+
+        return new Response('Creato link id ' . $link->getId());
     }
 }
