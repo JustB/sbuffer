4524eac9833754047b8095e9a54f7f6ae3bd1ae1
Giustino Borzacchiello <giustinob@gmail.com>
2014-09-18 00:01:46 +0200

    Added Link Entity

---
+++ src/Justb/LinksBundle/Entity/Link.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Justb\LinksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Link
+ *
+ * @package Justb\LinksBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="link")
+ */
+class Link
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $title;
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $url;
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Link
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Link
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Link
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
